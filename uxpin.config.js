174c7074ac25c9fe22b463fef1e814d27fd7bbb5
xrxoxcxox <watanuki@ju-zen.com>
2021-09-25 22:23:30 +0900

    Install UXPin

---
+++ uxpin.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  components: {
+    categories: [
+      {
+        name: 'General',
+        include: []
+      }
+    ],
+    webpackConfig: 'uxpin.webpack.config.js',
+  },
+  name: 'Learn UXPin Merge - React Todo list tutorial'
+};
